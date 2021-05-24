a3621ceabbb1c56db8466f32c9615ac12c83b841
Ildar Serazitdinov <serazitdinov.ildar@gmail.com>
2021-05-24 12:08:25 +0300

    4th step in codelab

--- app/src/main/java/com/countermeasure_software/composecodelab/MainActivity.kt
+++ app/src/main/java/com/countermeasure_software/composecodelab/MainActivity.kt
@@ -3,36 +3,56 @@ package com.countermeasure_software.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.countermeasure_software.composecodelab.ui.theme.ComposeCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeCodelabTheme {
+        Surface(color = Color.Green) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent() {
+    Column {
+        Greeting("Android Compose")
+        Divider(color = Color.White, thickness = 2.dp, startIndent = 24.dp)
+        Greeting("there")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeCodelabTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
